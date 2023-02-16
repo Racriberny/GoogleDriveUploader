1e9e89bbe6aa1e42d37c5131488c70c4e3002c2c
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-02-16 19:02:11 +0100

    cosas.

---
+++ src/main/java/com/joanjorgedanimichaeltobal/GoogleDriveUploader/GoogleDriveUploader.java
@@ -0,0 +1,18 @@
+package com.joanjorgedanimichaeltobal.GoogleDriveUploader;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import java.io.IOException;
+import java.util.Collections;
+
+
+public class GoogleDriveUploader {
+}
